e1614648811af392239b954116c639ae4d57db3e
Arun Vaishnav <arun.vaishnav@green-apex.com>
2020-12-09 09:53:42 +0530

    date time picker added

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,3 +1,4 @@
 class Channel < ApplicationRecord
 	has_many :shows
+	validates :name, presence: true
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -3,5 +3,8 @@ class Show < ApplicationRecord
 	has_many :favorites
 	has_many :users, through: :favorites
 
+	validates :title, presence: true
+	validates :time, presence: true
+
 	scope :search, -> (query) { joins(:channel).where("lower(title) LIKE ? OR lower(channels.name) LIKE ?", "%" + query.downcase + "%", "%" + query.downcase + "%") }
 end
